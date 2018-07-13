8dd218eed65fa286c463de2b2a14ca687371ec8c
Hassan A <has_is_ere@hotmail.com>
2018-07-13 14:35:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // add solution here
-function theBeatlesPlay (){
-  
+function theBeatlesPlay (musicians, instruments){
+  var arr = []
+  for (var i = 0; i < musicians.length -1; i++){
+      str += musicians[i] + " plays " + instruments[i]
+      arr.push(str)
+    }
+  }
 }
