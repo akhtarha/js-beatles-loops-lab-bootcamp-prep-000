bbdb0327d98d09ed7eafef2e7d2ffd07bdc906d2
Hassan A <has_is_ere@hotmail.com>
2018-07-13 14:36:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // add solution here
 function theBeatlesPlay (musicians, instruments){
   var arr = []
+  str = ""
   for (var i = 0; i < musicians.length -1; i++){
       str += musicians[i] + " plays " + instruments[i]
       arr.push(str)
-    }
+      str = ""
   }
+  return arr
+}
+
+function johnLennonFacts (arr){
+  
 }
